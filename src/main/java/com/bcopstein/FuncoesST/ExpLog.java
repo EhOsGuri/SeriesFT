41b81886d69f4a7d0b67b65747895cfd7c945445
17111168 <17111168@w3873320.portoalegre.pucrsnet.br>
2018-08-14 11:16:03 -0300

    "Metodo exp finalizado"

--- src/main/java/com/bcopstein/FuncoesST/ExpLog.java
+++ src/main/java/com/bcopstein/FuncoesST/ExpLog.java
@@ -5,19 +5,44 @@ import java.util.Iterator;
 import java.util.List;
 
 public class ExpLog {
-
+	
+	private List<Double> ex = new ArrayList<Double>();
+	
+	
 	public Iterator<Double> getTermos(){
-		// TODO:
-		throw new UnsupportedOperationException();
+		try{
+			return ex.iterator();
+		}catch(UnsupportedOperationException o) {
+			throw new UnsupportedOperationException();
+		}
 	}
 	
 	public double exp(double x) {
-		// TODO:
-		throw new UnsupportedOperationException();
+		try {
+			ex.clear();
+			double valor = 1;
+			int cont = 0;
+			while(valor > Math.pow(10, -6)) {
+				
+				double numElevado = Math.pow(x, cont);
+				valor = numElevado / Util.fatorial(cont);
+				ex.add(valor);
+				cont++;
+			}		
+		
+			return Util.somatorio(0,cont,ex); 
+			
+		}catch(UnsupportedOperationException o) {
+			throw new UnsupportedOperationException();
+		}
 	}
 
 	public double log(double x) {
-		// TODO:
-		throw new UnsupportedOperationException();
+		//try {
+			
+		//}catch(UnsupportedOperationException o) {
+			throw new UnsupportedOperationException();
+		//}
+		
 	}
 }
