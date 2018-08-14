7f8f0b192f85941914766161e2e64776ee2ba025
17111238 <17111238@w3873841.portoalegre.pucrsnet.br>
2018-08-14 11:09:00 -0300

    Implementação da classe Lambert

--- src/main/java/com/bcopstein/FuncoesST/Lambert.java
+++ src/main/java/com/bcopstein/FuncoesST/Lambert.java
@@ -4,14 +4,36 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import com.bcopstein.FuncoesST.Util;
+
 public class Lambert {
+	
+	public List<Double> list = new ArrayList<Double>();
+	
 	public Iterator<Double> getTermos(){
-		// TODO:
-		throw new UnsupportedOperationException();
+		return list.iterator();
 	}
 	
 	public double W0(double x) {
-		// TODO:
-		throw new UnsupportedOperationException();
+		int n = 1;
+		Double r = 0.0;
+		
+		try{
+			do {
+				Double w = (Math.pow(-n, n-1)/Util.fatorial(n))*Math.pow(x, n);
+				list.add(w);
+				n++;
+			}while(Math.abs(x)<(1/Math.E));
+			
+			for(int i=0; i<list.size();i++) {
+				r += list.get(i);
+			}
+	
+		}catch(IllegalArgumentException e){
+			System.out.println("Tipo de entrada invalida");
+		}
+		
+		
+		return r/list.size();
 	}
 }
